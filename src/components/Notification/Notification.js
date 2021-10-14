dc74d4879282823f34fab337f9e2d5505329e344
Brandon Warner <brandonswarner5@gmail.com>
2021-10-14 13:51:21 -0700

    uninstalled Toast Notifications -- not using them

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { ToastNotification } from 'carbon-components-react';
 import styled from 'styled-components';
 import { CSSTransition } from 'react-transition-group';
 import './Notification.css';
