0ca5342c285273ce36112faa69d624036f7e85b4
Brandon Warner <brandonswarner5@gmail.com>
2021-10-18 09:22:32 -0700

    added transitions for hamburger open / close

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Hamburger.css';
 
 const Hamburger = ({ hamburgerOpen }) => {
     console.log('hamburgerOpen: ', hamburgerOpen);
     return (
         <div className='hamburger'>
-            <div className='burger burger1' />
-            <div className='burger burger2' />
-            <div className='burger burger3' />
+            <Burger1 hamburgerOpen={hamburgerOpen} className='burger' />
+            <Burger2 hamburgerOpen={hamburgerOpen} className='burger' />
+            <Burger3 hamburgerOpen={hamburgerOpen} className='burger' />
         </div>
     );
 };
+
+const Burger1 = styled.div`
+    transform: ${props => (props.hamburgerOpen ? 'rotate(45deg)' : 'rotate(0)')};
+`;
+
+const Burger2 = styled.div`
+    transform: ${props => (props.hamburgerOpen ? 'translateX(100%)' : 'translateX(0)')};
+    opacity: ${props => (props.hamburgerOpen ? '0' : '1')};
+`;
+
+const Burger3 = styled.div`
+    transform: ${props => (props.hamburgerOpen ? 'rotate(-45deg)' : 'rotate(0)')};
+`;
 
 export default Hamburger;
